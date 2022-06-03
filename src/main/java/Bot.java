c62588690c1fe5dbf1953953d78217d3ad1dc599
Alex Calcan <79245328+atcalcan@users.noreply.github.com>
2022-06-03 20:45:01 +0300

    inceput

---
+++ src/main/java/Bot.java
@@ -0,0 +1,10 @@
+import net.dv8tion.jda.*;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Bot {
+    public static  void main(String args[]) throws Exception{
+
+        JDA jda = JDABuilder.createDefault("OTgyMzM0NzA2Mzc0ODExNjcw.GZH_oH.oe-6fxOYuGZEG0_EBT1DwCwq6ln5xdnJlK-egg").build();
+    }
+}
